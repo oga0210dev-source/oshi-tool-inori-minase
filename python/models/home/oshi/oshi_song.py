fde72780f4d9ffb13946da1c0ceee7c3b335aff0
ogaa5210 <ogaa5210.99@gmail.com>
2026-08-16 19:47:46 +0900

    推し情報（楽曲、ラジオ・番組）の追加

---
+++ python/models/home/oshi/oshi_song.py
@@ -0,0 +1,75 @@
+from python.core.database import get_connection
+
+
+def get_song_list(keyword=None, sort="album"):
+    conn = get_connection()
+
+    if sort == "song":
+        order_by = """
+            song_name ASC
+        """
+    elif sort == "release_desc":
+        order_by = """
+            release_date DESC NULLS FIRST,
+            album_name ASC,
+            display_order ASC,
+            song_name ASC
+        """
+    elif sort == "release_asc":
+        order_by = """
+            release_date ASC NULLS LAST,
+            album_name ASC,
+            display_order ASC,
+            song_name ASC
+        """
+    else:
+        order_by = """
+            release_date DESC NULLS FIRST,
+            album_name ASC,
+            display_order ASC,
+            song_name ASC
+        """
+
+    sql = """
+        SELECT
+            song_id,
+            song_name,
+            album_name,
+            release_date,
+            display_order,
+            tie_up,
+            youtube_url,
+            apple_music_url,
+            spotify_url
+        FROM m_song
+        WHERE
+            is_deleted = FALSE
+            AND is_public = TRUE
+    """
+
+    params = []
+
+    if keyword:
+        sql += """
+            AND (
+                song_name ILIKE %s
+                OR album_name ILIKE %s
+            )
+        """
+
+        params.extend([
+            f"%{keyword}%",
+            f"%{keyword}%"
+        ])
+
+    sql += f"""
+        ORDER BY
+        {order_by}
+    """
+
+    try:
+        with conn.cursor() as cur:
+            cur.execute(sql, params)
+            return cur.fetchall()
+    finally:
+        conn.close()
