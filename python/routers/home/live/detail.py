2d3976b71282686407d602d48e33c3171402db6c
ogaa5210 <ogaa5210.99@gmail.com>
2026-08-23 16:10:20 +0900

    インポートディレクトリの修正

--- python/routers/home/live/detail.py
+++ python/routers/home/live/detail.py
@@ -7,54 +7,50 @@ from fastapi.responses import RedirectResponse
 from python.core import auth
 from python.core import templates
 
-from python.models.home.meeting import detail as detail_model
+from python.models.home.live import detail as detail_model
 
 
 router = APIRouter(
-    prefix="/home/meeting/detail",
-    tags=["home_meeting_detail"]
+    prefix="/home/live/detail",
+    tags=["home_live_detail"]
 )
 
 
-@router.get("/{meeting_id}")
-async def meeting_detail(
+@router.get("/{live_id}")
+async def live_detail(
         request: Request,
-        meeting_id: int
+        live_id: int
 ):
     user_id = request.session.get("user_id")
 
-    meeting = detail_model.get_meeting_detail(
+    live = detail_model.get_live_detail(
         user_id,
-        meeting_id
+        live_id
     )
 
-    if meeting is None:
+    if live is None:
         return RedirectResponse(
-            "/home/meeting/archive",
+            "/home/live",
             status_code=303
         )
 
-    guests = detail_model.get_meeting_guest_list(
-        meeting_id
-    )
-
     today = date.today()
 
-    if meeting["meeting_date"] > today:
+    if live["live_date"] > today:
         days = (
-            meeting["meeting_date"] - today
+            live["live_date"] - today
         ).days
 
         day_status = f"あと{days}日"
 
-    elif meeting["meeting_date"] < today:
+    elif live["live_date"] < today:
         diff = relativedelta(
             today,
-            meeting["meeting_date"]
+            live["live_date"]
         )
 
         total_days = (
-            today - meeting["meeting_date"]
+            today - live["live_date"]
         ).days
 
         day_status = (
@@ -67,28 +63,13 @@ async def meeting_detail(
     else:
         day_status = "本日開催"
 
-    performance_type_map = {
-        "DAY": "昼公演",
-        "NIGHT": "夜公演",
-        "PART1": "第1部",
-        "PART2": "第2部",
-        "PART3": "第3部"
-    }
-
-    performance_type = performance_type_map.get(
-        meeting["performance_type"],
-        ""
-    )
-
     return templates.TemplateResponse(
         request=request,
-        name="templates/home/meeting/detail.html",
+        name="templates/home/live/detail.html",
         context={
-            "meeting": meeting,
-            "guests": guests,
+            "live": live,
             "today": today,
             "day_status": day_status,
-            "performance_type": performance_type,
             "is_login": auth.is_login(request)
         }
     )
